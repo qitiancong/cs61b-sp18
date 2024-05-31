bf27d8f9e690f4d81c165bb2de50569809c482fc
TIAN CONG QI <qitiancong@MacBook-Pro.lan>
2024-06-01 04:00:29 +0800

    fixed style check

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -6,7 +6,7 @@ public class LinkedListDeque<T> {
         private Node prev;
 
         /** constructor */
-        public Node(T item, Node prev, Node next) {
+        Node(T item, Node prev, Node next) {
             this.item = item;
             this.prev = prev;
             this.next = next;
@@ -145,7 +145,7 @@ public class LinkedListDeque<T> {
             }
         }
     }
-    
+
 }
 
 
