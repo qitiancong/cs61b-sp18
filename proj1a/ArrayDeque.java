ac2fba3bc98303d4e071f27475b5daa293288bb9
TIAN CONG QI <qitiancong@MacBook-Pro.lan>
2024-06-01 02:55:49 +0800

    change Deque implementation from circular to linear for easy tracking; Added print, remove; removed main.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,8 +3,6 @@ public class ArrayDeque<T> {
     private  int size;
     private int nextFirst; // index of next first item in current array.
     private int nextLast;
-    private int[] sequence; //keep track of the sequence of insertion,store index of underlying array.
-    private int sequence_index; // keep track of where to insert into sequence.
 
     /** constructor for an empty List */
     public ArrayDeque() {
@@ -17,7 +15,7 @@ public class ArrayDeque<T> {
     /** helper method. Resize underlying array to given capacity. */
     private  void resize(int capacity) {
         T[] new_array = (T[]) new Object[capacity];
-        System.arraycopy(this.array, 0, new_array, capacity / 4, this.size);
+        System.arraycopy(this.array, this.nextFirst + 1, new_array, capacity / 4, this.size);
         this.array = new_array;
         this.nextFirst = capacity / 4 - 1;
         this.nextLast = this.nextFirst + this.size + 1;
@@ -26,31 +24,23 @@ public class ArrayDeque<T> {
 
     public void addFirst(T item) {
         //TODO
-        //need to resize when array is full.
-        if (this.size == this.array.length) {
-            this.resize(this.size * 2);
+        //need to resize when start of array is reached. Not a circular array.
+        if (this.array[0] != null) {
+            this.resize(this.array.length * 2);
         }
         this.array[this.nextFirst] = item;
-        if (this.nextFirst == 0) {
-            this.nextFirst = this.array.length - 1; //Loop to the end of array.
-        } else {
-            this.nextFirst -= 1;
-        }
+        this.nextFirst -= 1;
         this.size += 1;
     }
 
     public void addLast(T item) {
         //TODO
-        //need to resize when array is full.
-        if (this.size == this.array.length) {
-            this.resize(this.size * 2);
+        //need to resize when end of array is reached. Not a circular array.
+        if (this.array[this.array.length - 1] != null) {
+            this.resize(this.array.length * 2);
         }
         this.array[this.nextLast] = item;
-        if(this.nextLast == this.array.length - 1) {
-            this.nextLast = 0; //Loop to the front of array.
-        } else {
-            this.nextLast += 1;
-        }
+        this.nextLast += 1;
         this.size += 1;
     }
 
@@ -66,45 +56,34 @@ public class ArrayDeque<T> {
 //
     public void printDeque() {
         //TODO
-        int index = this.nextLast - 1;  //caution: if nextFirst is at the back , will have problem.
+        int index = this.nextFirst + 1;
         while(this.array[index] != null) {
             System.out.print(this.array[index] + " ");
-            if (index == 0) { //index reached the end
-                index = this.array.length - 1; //start from the back again
-            } else {
-                index -= 1;
-            }
+            index += 1;
         }
     }
-//
-//    public T removeFirst() {
-//        //TODO
-//    }
-//
-//    public T removeLast() {
-//        //TODO
-//    }
-//
-//    public T get(int index) {
-//        //TODO
-//    }
 
-    public static void main (String[] args) {
-        ArrayDeque<Integer> s = new ArrayDeque<>();
-        s.addFirst(1);
-        s.addFirst(2);
-        s.addFirst(3);
-        s.addFirst(4);
-        s.addFirst(5);
-        s.addFirst(6);
-        s.addFirst(7);
-//        s.addFirst(8);
-//        s.addFirst(9);
-//        s.addFirst(10);
-//        s.addFirst(11);
-//        s.addFirst(12);
+    public T removeFirst() {
+        //TODO
+        T return_item = this.array[this.nextFirst + 1];
+        this.array[this.nextFirst + 1] = null;
+        this.nextFirst += 1;
+        this.size -= 1;
+        return return_item;
+    }
 
-        s.printDeque();
+    public T removeLast() {
+        //TODO
+        T return_item = this.array[this.nextLast - 1];
+        this.array[this.nextLast - 1] = null;
+        this.nextLast -= 1;
+        this.size -= 1;
+        return return_item;
+    }
+
+    public T get(int index) {
+        //TODO
+        return this.array[this.nextFirst + 1 + index];
     }
 
 }
